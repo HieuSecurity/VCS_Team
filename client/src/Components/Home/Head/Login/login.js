eb13edf7e231a895708209ab40d9c3f639c643e5
HieuSecurity <106015669+HieuSecurity@users.noreply.github.com>
2024-06-01 20:04:43 +0700

    hoàn thành việc đăng nhập

--- client/src/Components/Home/Head/Login/login.js
+++ client/src/Components/Home/Head/Login/login.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-function Login({ onLogin }) {
-  const handleLoginState = () => {
-    onLogin(true);
-  };
-
+function Login() {
   return (
     <div>
-      <Link style={{ textDecoration: "none" }} to="/login">
-        
+      <Link
+        style={{
+          textDecoration: "none",
+          backgroundColor: "rgb(220, 53, 69)",
+          borderRadius: "5px",
+          padding: "10px",
+          color: "white",
+        }}
+        to="/login"
+      >
+        Đăng nhập
       </Link>
     </div>
   );
--- client/src/Components/Home/Head/User/main.js
+++ client/src/Components/Home/Head/User/main.js
@@ -1,56 +1,14 @@
-<<<<<<< Updated upstream
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-=======
 import React, { useState, useContext } from "react";
->>>>>>> Stashed changes
 import { Link } from "react-router-dom";
 import "../User/style.css";
 import { StateContext } from "../MyProvider"; // Import context từ MyProvider
+
 function Main() {
   const [showModal, setShowModal] = useState(false);
-<<<<<<< Updated upstream
-  const [userName, setUserName] = useState("Người dùng");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    // Lấy dữ liệu từ localStorage
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user && user.EMAIL) {
-      getUserIdByEmail(user.EMAIL, (userId) => {
-        getUserInfo(userId, (userInfo) => {
-          setUserName(userInfo.NAME); // Thông tin người dùng có thuộc tính 'name'
-          setIsLoggedIn(true);
-        });
-      });
-    }
-  }, []);
-
-  const getUserIdByEmail = (email, callback) => {
-    axios
-      .get(`http://localhost:3000/api/get-userid-byEmail/${email}`)
-      .then((response) => {
-        callback(response.data.USERID); // Giả sử phản hồi chứa USERID
-      })
-      .catch((error) => {
-        console.error("Error fetching USERID:", error);
-      });
-  };
-
-  const getUserInfo = (userId, callback) => {
-    axios
-      .get(`http://localhost:3000/api/user-info/${userId}`)
-      .then((response) => {
-        callback(response.data); // Giả sử phản hồi chứa thông tin người dùng
-      })
-      .catch((error) => {
-        console.error("Error fetching user info:", error);
-      });
-  };
-=======
   const { state, setState } = useContext(StateContext); // Sử dụng context từ MyProvider
->>>>>>> Stashed changes
-
+  const handleLogout = () => {
+    setState(false);
+  };
   const showUserForm = () => {
     setShowModal(true);
     document.body.classList.add("modal-active"); // Thêm class để ngăn cuộn trang khi modal hiển thị
@@ -67,37 +25,24 @@ function Main() {
     }
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem("user");
-    setIsLoggedIn(false);
-    setUserName("Người dùng");
-    hideUserForm();
-  };
-
   return (
     <div className={`Main ${showModal ? "modal-active" : ""}`}>
-      {isLoggedIn ? (
-        <div onClick={showUserForm} className="user">
-          <img
-            style={{ height: "70px", borderRadius: "50%" }}
-            src="https://t4.ftcdn.net/jpg/03/49/49/79/360_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
-            alt="User Avatar"
-          />
-          <span
-            style={{
-              marginRight: "2px",
-              marginLeft: "10px",
-              fontWeight: "700",
-            }}
-          >
-            {userName}
-          </span>
-        </div>
-      ) : (
-        <Link to="/login" className="login-button">
-          Đăng nhập
-        </Link>
-      )}
+      <div onClick={showUserForm} className="user">
+        <img
+          style={{ height: "70px", borderRadius: "50%" }}
+          src="https://t4.ftcdn.net/jpg/03/49/49/79/360_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
+          alt="User Avatar"
+        />
+        <span
+          style={{
+            marginRight: "2px",
+            marginLeft: "10px",
+            fontWeight: "700",
+          }}
+        >
+          Nguyễn Minh Hiếu
+        </span>
+      </div>
       {showModal && (
         <div className="overlay" onClick={handleOverlayClick}></div>
       )}
@@ -111,16 +56,12 @@ function Main() {
             className="modal-content"
           >
             <Link to="/user/info" style={{ margin: "15px 0" }}>
-              Xem thông tin
+              Xem thông tin{" "}
             </Link>
             <Link to="/changepassword" style={{ margin: "15px 0" }}>
               Thay đổi mật khẩu
             </Link>
-<<<<<<< Updated upstream
-            <Link to="/" onClick={handleLogout} style={{ margin: "15px 0" }}>
-=======
-            <Link to="/login" style={{ margin: "15px 0" }}>
->>>>>>> Stashed changes
+            <Link to="/" style={{ margin: "15px 0" }} onClick={handleLogout}>
               Đăng xuất
             </Link>
             <span
